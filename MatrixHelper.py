c30467ad13a3ad331693ccda9b582973cdead394
Tomas Morris <tomas.morris13@gmail.com>
2012-11-30 16:45:36 -0700

    Some testing and trimming of possible orders

--- MatrixHelper.py
+++ MatrixHelper.py
@@ -2,6 +2,8 @@
 
 from itertools import *
 from sys import maxint
+from multiprocessing import cpu_count
+import threading
 
 usedList = {}
 
@@ -16,7 +18,7 @@ def getMatrix(fileName):
 	return allRows
 
 def getDistMatrix(num):
-	allDists = list() 
+	allDists = list()
 	for i in range(0,num):
 		thisDist = list()
 		for j in range(-i,num-i):
@@ -24,16 +26,21 @@ def getDistMatrix(num):
 		allDists.append(thisDist)
 	return allDists
 
-def solve(matrix): 
-	orders = permutations(range(1,len(matrix)+1))
-	shortestDistance = maxint
+def solve(matrix):
+	orders = permutations(range(1,len(matrix)+1)) # All Possible
 	distMatrix = getDistMatrix(len(matrix))
+	if(len(matrix) > 8):
+		orders = breakAbsoluteSolution(orders, matrix)
+	print(len(orders))
+	shortestDistance = maxint
 	correctOrder = list()
+	numDone = 0
 	for order in orders:
-		dist = getDist(order, matrix, distMatrix)
+		dist = getDist(order[::-1], matrix, distMatrix) # taking advantage of lexigraphic ordering
 		if(dist < shortestDistance):
 			correctOrder = order
 			shortestDistance = dist
+	printMatrix(correctOrder, matrix)
 	return [correctOrder, shortestDistance]
 
 def getDist(order, matrix, distMatrix):
@@ -42,14 +49,64 @@ def getDist(order, matrix, distMatrix):
 	for i in range(0,len(order)):
 		whatsLeft = ''.join(str(x) for x in order[i:])
 		if (whatsLeft in usedList):
-			print "Was Dynamic"
 			dist += usedList[whatsLeft]
 			break
 		else:
-			print "Wasn't Dynamic"
 			thisDist = 0
 			for j in range(i,len(distMatrix[i])):
 				thisDist += matrix[order[i]-1][order[j]-1] * distMatrix[i][j]
 			usedList[whatsLeft] = thisDist
 			dist += thisDist
 	return dist	
+
+def printMatrix(order, matrix):
+	newMatrix = reorderMatrix(order, matrix)
+	for down in newMatrix:
+		for over in down:
+			print str(over),
+		print
+	print
+
+def reorderMatrix(order, matrix):
+	newMatrix = list()
+	for down in order:
+		row = list()
+		for over in order:
+			row.append(str(matrix[down-1][over-1]))
+		newMatrix.append(row)
+	return newMatrix
+
+#Assumption! Higher values near the diagonal are more likely to render the solution
+def breakAbsoluteSolution(orders, matrix):
+	slimmedOrders = list()
+	orders = HighestTogether(orders)
+	for order in slimmedOrders:
+		thisDistMatrix = reorderMatrix(order, matrix)
+		if(hasHighestOnDiagonal(thisDistMatrix)):
+			slimmedOrders.append(order)
+	return slimmedOrders
+
+def highestTogether(orders, matrix):
+	i = -1
+	values = [sumRow(x, i) for x in matrix]
+	values = sorted(values, key = lambda row : row[1])		
+
+def sumRow(row, i):
+	i += 1
+	return (sum([int(x) for x in row]),i)  
+
+def hasHighestOnDiagonal(distMatrix):
+	numToCheck = int(.2 * len(distMatrix))
+	itDoes = True
+	for i in range(0,len(distMatrix)):
+		thisOneDoes = False
+		highest = 0
+		for el in distMatrix[i]:
+			if(highest < el):
+				highest = el
+		for j in (i-numToCheck,i+numToCheck+1):
+			if(j > -1 and j < len(distMatrix)):
+				if(distMatrix[i][j] == highest):
+					thisOneDoes = True
+		itDoes = itDoes and thisOneDoes
+	return itDoes
--- outputCreator.py
+++ outputCreator.py
@@ -9,8 +9,16 @@ numElements = int(argv[1])
 testFile = open("test"+str(numElements),'w')
 
 testFile.write(str(numElements)+"\n")
+upperHalf = list()
 for i in range(0,numElements):
-	for j in range(0,numElements):
-		testFile.write(str(randint(0,5))+" ")
-	testFile.write("\n")
+	row = list()
+	for j in range(i+1,numElements):
+		row.append(str(randint(0,100)))
+	upperHalf.append(row)
 
+for i in range(0, numElements):
+	upperHalf[i].insert(0,"0")
+	for j in range(0,i):
+		upperHalf[i].insert(j ,upperHalf[j][i])
+
+testFile.write('\n'.join(' '.join(x) for x in upperHalf))
